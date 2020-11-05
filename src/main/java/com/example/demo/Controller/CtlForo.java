8a52e4d4873114da2ebe20996496eec1d0017d3f
Alejandro Lopez <edwardlopez625@gmail.com>
2020-11-05 04:15:06 -0500

    Ya registra foros, pendiente el listar foros, y en docentes, no se registran los docentes

--- src/main/java/com/example/demo/Controller/CtlForo.java
+++ src/main/java/com/example/demo/Controller/CtlForo.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.Estudiante;
 import com.example.demo.Entity.Foro;
 import com.example.demo.Repository.RepoForo;
 
@@ -35,5 +38,15 @@ public class CtlForo {
 	@GetMapping("/listarForos")
 	public List<Foro> listarForos() {
 		return this.repoForo.findAll();
+	}
+
+	@DeleteMapping("eliminarForo/{id}")
+	public ResponseEntity<Foro> eliminar(@PathVariable("id") int id) {
+		Foro foro = this.repoForo.findById(id);
+		if (foro == null)
+			return ResponseEntity.notFound().build();
+		else
+			this.repoForo.delete(foro);
+		return ResponseEntity.ok(foro);
 	}
 }
